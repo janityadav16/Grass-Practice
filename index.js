7c75dbd58eb6db259a8d193178e22bff5eb34ac6
Janit Yadav <janityadav16@gmail.com>
2024-09-20 22:32:54 +0530

    to practice js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+// const myMap = new Map([
+//     ['name', 'John'],
+//     ['age', 30],
+//     ['occupation', 'Developer']
+// ]);
+
+
+// console.log(myMap.get('name'));
+
+// for (let [key, value] of myMap) {
+//     console.log(`${key}: ${value}`);
+// }
+
+
+
 const myMap = new Map([
     ['name', 'John'],
     ['age', 30],
-    ['occupation', 'Developer']
+    ['occupation', 'Developer'],
+    ['salary', 5000]
 ]);
 
+const filteredMap = new Map(
+    [...myMap].filter(([key, value]) => typeof value === 'number')
+);
 
-console.log(myMap.get('name'));
-
-for (let [key, value] of myMap) {
+filteredMap.forEach((value, key) => {
     console.log(`${key}: ${value}`);
-}
+});
