9be569920831ff96de26dd7ea2f63e3663b4d25c
Janit Yadav <janityadav16@gmail.com>
2024-09-21 20:10:57 +0530

    to practice js

--- index.js
+++ index.js
@@ -13,17 +13,26 @@
 
 
 
-const myMap = new Map([
-    ['name', 'John'],
-    ['age', 30],
-    ['occupation', 'Developer'],
-    ['salary', 5000]
-]);
+// const myMap = new Map([
+//     ['name', 'John'],
+//     ['age', 30],
+//     ['occupation', 'Developer'],
+//     ['salary', 5000]
+// ]);
 
-const filteredMap = new Map(
-    [...myMap].filter(([key, value]) => typeof value === 'number')
-);
+// const filteredMap = new Map(
+//     [...myMap].filter(([key, value]) => typeof value === 'number')
+// );
 
-filteredMap.forEach((value, key) => {
-    console.log(`${key}: ${value}`);
-});
+// filteredMap.forEach((value, key) => {
+//     console.log(`${key}: ${value}`);
+// });
+
+const points = [40, 100, 1, 5, 25, 10];
+
+for (let i = points.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let k = points[i];
+    points[i] = points[j];
+    points[j] = k;
+}
