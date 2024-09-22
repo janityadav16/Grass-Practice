c2c7463f51ada67a1b842b0267d8889545b84a70
Janit Yadav <janityadav16@gmail.com>
2024-09-22 22:00:48 +0530

    to practice js

--- index.js
+++ index.js
@@ -30,9 +30,5 @@
 
 const points = [40, 100, 1, 5, 25, 10];
 
-for (let i = points.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let k = points[i];
-    points[i] = points[j];
-    points[j] = k;
-}
+const five = points.filter(point => point === 5);
+console.log(five);
