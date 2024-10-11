0e94472ed16bc18b11afe9903dbf3248b920b21b
Janit Yadav <janityadav16@gmail.com>
2024-10-11 14:54:13 +0530

    to practice js

--- promise.js
+++ promise.js
@@ -1,22 +1,21 @@
-// function createPromise(startTime, timeout) {
-//     return new Promise((resolve, reject) => {
-//         const timerId = setTimeout(() => {
-//             const succeeded = Date.now() - startTime < timeout;
-//             if (succeeded) {
-//                 resolve('Promise resolved');
-//             } else {
-//                 reject('Promise rejected');
-//             }
-//         }, timeout);
-//         return () => clearTimeout(timerId);
-//     });
-// }
+const createPromise = (startTime, timeout) => new Promise((resolve, reject) => {
+    const timerId = setTimeout(() => {
+        const succeeded = Date.now() - startTime < timeout;
+        succeeded ? resolve('Promise resolved') : reject('Promise rejected');
+    }, timeout);
+
+    return () => clearTimeout(timerId);
+});
+
 let count = 0;
+
 document.getElementById('plus').addEventListener('click', () => {
     count++;
     document.getElementById('count').innerHTML = count;
 });
+
 document.getElementById('minus').addEventListener('click', () => {
     count--;
     document.getElementById('count').innerHTML = count;
 });
+
