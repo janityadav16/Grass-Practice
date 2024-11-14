834ca4157d42fca49d2833482da08a4fb60cf6d6
Janit Yadav <janityadav16@gmail.com>
2024-11-14 21:52:17 +0530

    small changes

--- fetch.js
+++ fetch.js
@@ -1,14 +1,20 @@
 fetch('https://fakestoreapi.com/products')
     .then(response => {
-        if (!response.ok) {
+        // Check if the response was successful
+        if (response.ok) {
+            // If the response was successful, return the response as JSON
+            return response.json();
+        } else {
+            // If the response was not successful, throw an error
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        return response.json();
     })
     .then(products => {
+        // Log the products to the console
         console.log(products);
     })
     .catch(error => {
+        // Log any errors to the console
         console.error('Error fetching products:', error);
     });
 
